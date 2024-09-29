4bc6ba6aa446d24438f7d9aaa81939343bd79f7d
bln51000 <shanegibson1221@gmail.com>
2024-09-29 18:34:27 -0400

    Initial commit

---
+++ Assets/RocketMotion.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMotion : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+    AudioSource audioSource;
+    [SerializeField] float rocketThrust = 100f;
+    [SerializeField] float rocketRotation = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //Debug.Log("Space bar is pushed.");
+            rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * rocketThrust);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+        }else{
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            //Debug.Log("A is pushed.");
+            transform.Rotate(Vector3.forward * Time.deltaTime * rocketRotation);
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            //Debug.Log("D is pushed.");
+            transform.Rotate(Vector3.back * Time.deltaTime * rocketRotation);
+        }
+    }
+}
